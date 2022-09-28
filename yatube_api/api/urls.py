a71968f5f18d7445b0000cd81e57ceaae6e5958a
ash1k-dev <ash1kekb@yandex.ru>
2022-09-29 01:27:18 +0500

    6

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,11 +7,13 @@ router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(
-    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
-router.register(r'follow', FollowViewSet)
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
